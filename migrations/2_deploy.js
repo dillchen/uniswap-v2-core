b6cb79c34c28c4640db7c45f8712559abb8fe865
Jake Naviasky <jake@commonwealth.im>
2020-09-04 09:50:04 -0400

    Configure truffle with edgeware provider.

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -2,6 +2,6 @@ const UniswapV2Factory = artifacts.require("UniswapV2Factory");
 
 const FEE_TO_SETTER_ADDRESS = "0x0000000000000000000000000000000000000000"
 
-module.exports = function (deployer) {
-  deployer.deploy(UniswapV2Factory, FEE_TO_SETTER_ADDRESS);
+module.exports = async function (deployer, network, accounts) {
+  await deployer.deploy(UniswapV2Factory, FEE_TO_SETTER_ADDRESS, { from: accounts[0], gas: 50000000 });
 };
