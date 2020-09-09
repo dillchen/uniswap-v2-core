a64323f8cdd810b8582cb616491a047ad1c9aa4a
dillchen <dillon@commonwealth.im>
2020-09-09 13:32:13 -0400

    add weth9

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -1,7 +1,9 @@
 const UniswapV2Factory = artifacts.require("UniswapV2Factory");
+const WETH9 = artifacts.require("WETH9");
 
 const FEE_TO_SETTER_ADDRESS = "0x0000000000000000000000000000000000000000"
 
 module.exports = async function (deployer, network, accounts) {
+  await deployer.deploy(WETH9, { from: accounts[0], gas: 50000000 });
   await deployer.deploy(UniswapV2Factory, FEE_TO_SETTER_ADDRESS, { from: accounts[0], gas: 50000000 });
 };
